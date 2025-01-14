3061fab5e03da159fb820a967782d34468af1b29
Isaaja <111202214416@mhs.dinus.ac.id>
2025-01-14 18:06:03 +0700

    update products

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,12 +6,12 @@ const Product = ({ product }) => {
 
   return (
     <>
-      <div className="w-32 md:w-48 lg:w-56 max-w-screen-xl ">
+      <div className="w-32 md:w-48 lg:w-56 max-w-screen-xl mb-3 relative sm:mb-7">
         <Link to={`/product/${id}`}>
           <div className="border border-[#e4e4e4]  mb-4 relative overflow-hidden group transition">
             <div className="w-full h-full flex justify-center items-center">
               {/* image */}
-              <div className="w-[200px] mx-auto flex justify-center items-center">
+              <div className="w-[200px] mx-auto flex justify-center items-center ">
                 <img
                   className="max-h-[160px] group-hover:scale-110 transition duration-300"
                   src={image}
@@ -22,10 +22,12 @@ const Product = ({ product }) => {
           </div>
         </Link>
         {/* category, title & price */}
-        <div className="text-xs capitalize text-gray-500 mb-1">{category}</div>
-        <h2 className="text-sm mb-1">{title}</h2>
-        <h2 className="font-semibold">Rp{price}</h2>
-      </div>
+          <div className="text-xs capitalize text-gray-500 mb-1">
+            {category}
+          </div>
+          <h2 className="text-sm mb-8">{title}</h2>
+          <h2 className="font-semibold absolute bottom-0">Rp{price}</h2>
+        </div>
     </>
   );
 };
